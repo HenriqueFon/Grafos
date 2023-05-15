e7e1f3746a613ae4d34c58eb39356a757226beac
Henrique Fonseca Araújo <67172006+HenriqueFon@users.noreply.github.com>
2023-05-15 19:10:10 -0300

    Add files via upload

---
+++ src/src/model/Pessoa.java
@@ -0,0 +1,11 @@
+package model;
+
+public class Pessoa {
+    public String Nome;
+    public int Idade;
+
+    public Pessoa(String _nome, int _idade) {
+        Nome = _nome;
+        Idade = _idade;
+    }
+}
